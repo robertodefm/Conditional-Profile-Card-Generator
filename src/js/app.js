005e428a6bbeb63414d0871db76bfa47e03d079e
robertodefm <robertodefm@gmail.com>
2022-07-26 01:06:49 +0000

    hola

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,15 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name + " " + variables.lastname}</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? " " : variables.name + " "} 
+          ${variables.lastname == null ? " " : variables.lastname}</h1>
+          <h2>${variables.role == null ? " " : variables.role}</h2>
+          <h3>${variables.city == null ? " " : variables.city + ", "}  ${variables.country == null ? " " : variables.country}</h3>
+          <ul class="${variables.socialMediaPosition == null ? "" : variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter == null ? "" : variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github == null ? "" : variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin == null ? "" : variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram == null ? "" : variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
